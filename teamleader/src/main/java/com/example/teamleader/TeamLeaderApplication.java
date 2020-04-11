891eaf747636880d74c9fa1d27c9bdfc6511b362
Tobiasz <tobiasz.rumian@hotmail.com>
2020-04-11 18:35:31 +0200

    Add rest of necessary modules

---
+++ teamleader/src/main/java/com/example/teamleader/TeamLeaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamleader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamLeaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamLeaderApplication.class, args);
+    }
+
+}
